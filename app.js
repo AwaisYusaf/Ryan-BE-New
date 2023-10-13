3c6299c7dfe36ac674ca3eaaae12526cbc705f9c
AwaisYusaf <muhammadawais6640@gmail.com>
2023-10-13 20:25:06 +0500

    Movies router export updated to module.exports

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const moviesRouter = require("./routes/moviesRouter");
 const cors = require("cors");
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use("/api/movies", moviesRouter);
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 app.listen(5000, () => console.log(""));
+
+module.exports = app;
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -1,10 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { Router } = require("express");
-const { GetMovies, AddMovie, EditMovie, DeleteMovie, PrepareCSV, PrepareTXT, GetMovieByID } = require("../controllers/moviesControllers");
+const {
+  GetMovies,
+  AddMovie,
+  EditMovie,
+  DeleteMovie,
+  PrepareCSV,
+  PrepareTXT,
+  GetMovieByID,
+} = require("../controllers/moviesControllers");
 const moviesRouter = Router();
 moviesRouter.route("/").get(GetMovies).post(AddMovie).put(EditMovie);
 moviesRouter.route("/:id").get(GetMovieByID).delete(DeleteMovie);
 moviesRouter.route("/download-csv").get(PrepareCSV);
 moviesRouter.route("/download-txt").get(PrepareTXT);
-exports.default = moviesRouter;
+
+module.exports = moviesRouter;
